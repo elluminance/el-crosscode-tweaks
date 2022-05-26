7ac9b684a945210573a29d4538e26a49f3b75258
EL2020 <el2020202020@gmail.com>
2022-05-26 15:56:32 -0400

    moved many action steps here

--- src/headers/el-tweaks.d.ts
+++ src/headers/el-tweaks.d.ts
@@ -194,6 +194,72 @@ declare global {
                 autoThrow: true;
             }
         }
+
+        namespace ACTION_STEP {
+            namespace EL_SET_TARGET {
+                interface Settings {
+                    name: string
+                }
+            }
+            interface EL_SET_TARGET extends ig.ActionStepBase {
+                name: string;
+                start(this: this, target: ig.ENTITY.Combatant): void
+            }
+            interface EL_SET_TARGET_CONSTRUCTOR extends ImpactClass<EL_SET_TARGET> {
+                new (settings: EL_SET_TARGET.Settings): EL_SET_TARGET
+            }
+            var EL_SET_TARGET: EL_SET_TARGET_CONSTRUCTOR;
+
+
+            interface EL_ELEMENT_IF extends ig.ActionStepBase {
+                branches: Record<string, ig.ActionStepBase>;
+                getBranchNames(this: this): string[];
+                getNext(this: this, entity: ig.ENTITY.Combatant): ig.ActionStepBase
+            }
+            interface EL_ELEMENT_IF_CONSTRUCTOR extends ImpactClass<EL_ELEMENT_IF> {
+                new (): EL_ELEMENT_IF;
+            }
+            var EL_ELEMENT_IF: EL_ELEMENT_IF_CONSTRUCTOR;
+
+
+            namespace GOTO_LABEL_WHILE {
+                interface Settings {
+                    name: string;
+                    condition: string;
+                }
+            }
+            interface GOTO_LABEL_WHILE extends ig.ActionStepBase {
+                name: string;
+                condition: ig.VarCondition;
+
+                getJumpLabelName(this: this): string | null;
+            }
+            interface GOTO_LABEL_WHILE_CONSTRUCTOR extends ImpactClass<GOTO_LABEL_WHILE> {
+                new (settings: GOTO_LABEL_WHILE.Settings): GOTO_LABEL_WHILE;
+            }
+            var GOTO_LABEL_WHILE: GOTO_LABEL_WHILE_CONSTRUCTOR
+
+
+            namespace SET_ATTRIB_CURRENT_POS {
+                interface Settings {
+                    attrib: string;
+                }
+            }
+            interface SET_ATTRIB_CURRENT_POS extends ig.ActionStepBase {
+                attrib: string;
+            }
+            interface SET_ATTRIB_CURRENT_POS_CONSTRUCTOR extends ImpactClass<SET_ATTRIB_CURRENT_POS> {
+                new (settings: SET_ATTRIB_CURRENT_POS.Settings): SET_ATTRIB_CURRENT_POS;
+            }
+            var SET_ATTRIB_CURRENT_POS: SET_ATTRIB_CURRENT_POS_CONSTRUCTOR
+
+
+            namespace SET_CLOSE_TEMP_TARGET {
+                interface Settings {
+                    customSearchType?: SearchType
+                }
+            }
+        }
     }
 
     interface Window {
--- src/prestart/steps/action-steps.ts
+++ src/prestart/steps/action-steps.ts
@@ -16,3 +16,80 @@ ig.ACTION_STEP.SET_PLAYER_INVINCIBLE.inject({
         return true
     }
 })
+
+ig.ACTION_STEP.EL_ELEMENT_IF = ig.ActionStepBase.extend({
+    branches: {},
+    init() { },
+    getBranchNames() {
+        return ["neutral", "heat", "cold", "shock", "wave"]
+    },
+    getNext(entity) {
+        switch (sc.combat.getElementMode(entity)) {
+            case sc.ELEMENT.NEUTRAL:
+                return this.branches.neutral
+            case sc.ELEMENT.HEAT:
+                return this.branches.heat
+            case sc.ELEMENT.COLD:
+                return this.branches.cold
+            case sc.ELEMENT.SHOCK:
+                return this.branches.shock
+            case sc.ELEMENT.WAVE:
+                return this.branches.wave
+        }
+    }
+})
+
+ig.ACTION_STEP.GOTO_LABEL_WHILE = ig.ActionStepBase.extend({
+    name: "",
+    condition: null,
+
+    init(settings) {
+        this.name = settings.name;
+        this.condition = new ig.VarCondition(settings.condition);
+    },
+
+    getJumpLabelName() {
+        return this.condition.evaluate() ? this.name : null;
+    }
+})
+
+ig.ACTION_STEP.SET_TEMP_TARGET.inject({
+    init(settings) {
+        this.parent(settings);
+        //@ts-expect-error
+        if (settings.kind == "LAST_HIT") {
+            this.kind = function (combatant) {
+                return combatant.combo.hitCombatants.last();
+            }
+        }
+    }
+})
+
+ig.ACTION_STEP.EL_SET_TARGET = ig.ActionStepBase.extend({
+    init: function (a) {
+        this.name = a.name
+    },
+    start: function (target) {
+        target.setTarget(ig.game.getEntityByName(this.name) || null)
+    }
+})
+
+ig.ACTION_STEP.SET_ATTRIB_CURRENT_POS = ig.ActionStepBase.extend({
+    init(settings) {
+        this.attrib = settings.attrib
+    },
+
+    start(entity) {
+        entity && entity.setAttribute(this.attrib, entity.getAlignedPos(ig.ENTITY_ALIGN.BOTTOM))
+    }
+})
+
+ig.ACTION_STEP.SET_CLOSE_TEMP_TARGET.inject({
+    init(settings) {
+        this.parent(settings);
+
+        if(settings.searchType == "CUSTOM" && settings.customSearchType) {
+            this.searchType = settings.customSearchType;
+        }
+    }
+})
