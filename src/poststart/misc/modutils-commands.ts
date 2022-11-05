8c21e4b8e8e9b464769ece68ef2fa9ffcbf62922
EL2020 <el2020202020@gmail.com>
2022-11-05 19:38:24 -0400

    added spawn/reload enemy commands

--- src/poststart/misc/modutils-commands.ts
+++ src/poststart/misc/modutils-commands.ts
@@ -13,7 +13,9 @@ window.cmd = {
         }
     },
     addCredits: (amount: number) => sc.model.player.addCredit(amount),
+
     teleport: (mapName: string, destination?: ig.TeleportPosition) => ig.game.teleport(mapName, destination),
+    
     /*
      * will reload all player configs to reflect any changes
      * for example, if you're tweaking an art, you don't need to reload the game
@@ -34,5 +36,40 @@ window.cmd = {
                 sheet.reload()
             } catch {};
         })
-    }
+    },
+    reloadEnemyType(enemyName: string, reloadEffects: boolean = true) {
+        let enemyType = sc.EnemyType.cache[enemyName];
+        if(enemyType) {
+            enemyType.debugReload = true;
+            enemyType.reload();
+        }
+
+        if(reloadEffects) this.reloadEffectSheets();
+    },
+
+    spawnEnemy(enemyName: string, levelOverride?: number, settings?: sc.EnemyInfo.Settings, pos?: Vec3) {
+        //@ts-expect-error
+        settings ??= {};
+        
+        settings!.type = enemyName;
+        settings!.level = levelOverride;
+        
+        let enemyInfo = new sc.EnemyInfo(settings!);
+        
+        pos ??= {...ig.game.playerEntity.coll.pos}
+
+        //when an enemy is first loading, the size information isn't immediately available.
+        //so to "counter" this - simply delay execution if size isn't defined.
+        setTimeout(() => {            
+            pos!.x -=  enemyInfo.enemyType.size.x / 2;
+            pos!.y -=  enemyInfo.enemyType.size.y / 2;
+    
+            ig.game.spawnEntity(
+                ig.ENTITY.Enemy, 
+                pos!.x, pos!.y, pos!.z, 
+                {enemyInfo: enemyInfo.getSettings()},
+                true
+            )
+        }, enemyInfo.enemyType.size ? 0 : 50);
+    },
 }
