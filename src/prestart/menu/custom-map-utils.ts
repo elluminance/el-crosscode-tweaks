ff098d34abf0a1f44baad47552d9c693be17ed62
krypek <krypek@tuta.io>
2023-09-28 23:23:53 +0200

    Add support for out of bounds area map entries

--- src/prestart/menu/custom-map-utils.ts
+++ src/prestart/menu/custom-map-utils.ts
@@ -1,6 +1,56 @@
 sc.AreaLoadable.inject({
     onload(data) {
         for (let floor of data.floors) {
+            if (floor.tiles.length == 0 || floor.tiles[0].length == 0) { continue }
+            const min: Vec2 = Vec2.createC(0, 0)
+            const os: Vec2 = Vec2.createC(floor.tiles[0].length, floor.tiles.length)
+            const max: Vec2 = Vec2.createC(os.x, os.y)
+            for (const map of floor.maps) {
+                if (map.customMap) {
+                    const c: sc.AreaLoadable.CustomMap = map.customMap
+                    min.x = Math.min(min.x, c.offX)
+                    min.y = Math.min(min.y, c.offY)
+                    max.x = Math.max(max.x, c.offX + c.mapData[0].length)
+                    max.y = Math.max(max.y, c.offY + c.mapData.length)
+                }
+            }
+            if (min.x != 0 || min.y != 0 || max.x != os.x || max.y != os.y) {
+                console.log(floor, min, max)
+                const newSize: Vec2 = Vec2.create(max)
+                Vec2.sub(newSize, min) /* min has to be negative here */
+                /* create an empty array */
+                const arr: number[][] = []
+                for (let y = 0; y < newSize.y; y++) {
+                    arr[y] = []
+                    for (let x = 0; x < newSize.x; x++) {
+                        arr[y][x] = 0
+                    }
+                }
+
+                /* paste original with offset */
+                for (let y = -min.y; y < os.y + -min.y; y++) {
+                    for (let x = -min.x; x < os.x + -min.x; x++) {
+                        arr[y][x] = floor.tiles[y + min.y][x + min.x]
+                    }
+                }
+                floor.tiles = arr
+                /* offset other stuff */
+                for (const map of floor.maps) {
+                    if (map.customMap) {
+                        map.customMap.offX -= min.x
+                        map.customMap.offY -= min.y
+                    }
+                }
+                for (const conn of floor.connections) { conn.tx -= min.x; conn.ty -= min.y; }
+                const minE: Vec2 = Vec2.create(min)
+                Vec2.mulC(minE, 8)
+                for (const icon of floor.icons) { icon.x -= minE.x; icon.y -= minE.y; }
+                for (const landmark of floor.landmarks) { landmark.x -= minE.x; landmark.y -= minE.y; }
+            }
+        }
+
+
+        for (let floor of data.floors) {
             let mapIdLookup: Record<string, number> = {};
             for (let i = 0; i < floor.maps.length; i++) {
                 let map = floor.maps[i];
